512fc54b1306e105946087484bc6fa79a6fedea6
Jared Hance <jaredhance@gmail.com>
2012-07-22 17:48:01 -0400

    Add support for getting all gists of a user

--- Web/GitHub/Gist.hs
+++ Web/GitHub/Gist.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE FlexibleContexts, OverloadedStrings #-}
 module Web.GitHub.Gist
     (
-    Gist(..)
+    Gist(..),
+    getGist,
+    getGists,
+    gists
     )
 where
 
@@ -11,6 +14,8 @@ import Control.Monad
 import Control.Monad.Trans.Control
 import Control.Monad.Trans.Resource
 import Data.Aeson
+import Data.Conduit
+import qualified Data.Conduit.List as CL
 import qualified Data.Text as T
 import Network.HTTP.Conduit
 
@@ -55,12 +60,45 @@ instance FromJSON Gist where
 
     parseJSON _ = mzero
 
+instance FromJSON GistUser where
+    parseJSON (Object o) = GistUser
+        <$> o .: "login"
+        <*> o .: "id"
+        <*> o .: "avatar_url"
+        <*> o .: "gravatar_id"
+        <*> o .: "url"
+
+    parseJSON _ = mzero
+
+jsonToGist :: Value -> Gist
+jsonToGist val = case fromJSON val of
+                    Success gist -> gist
+                    Error err -> error err
+
+-- | Gets a gist by ID.
 getGist :: (Failure HttpException m, MonadResource m, MonadBaseControl IO m)
         => Integer 
         -> Manager
         -> m Gist
 getGist id m = do req <- parseUrl $ "https://api.github.com/gists/" ++ show id
                   (val, headers) <- simpleRequest req m
-                  case fromJSON val of
-                    Success gist -> return gist
-                    Error err -> error err
+                  return $ jsonToGist val
+
+-- | Gets a list of all Gists of a user. Equivalent to
+-- 'https://api.github.com/users/:user/gists'
+-- 
+-- As long as there is a next rel, it will continue to fetch more gists. For
+-- more efficiency, use `gists`
+getGists :: (Failure HttpException m, MonadResource m, MonadBaseControl IO m)
+         => String
+         -> Manager
+         -> m [Gist]
+getGists user m = gists user m $$ CL.consume
+
+-- | Source that obtains all gists of a user with the specified username.
+gists :: (Failure HttpException m, MonadResource m, MonadBaseControl IO m)
+      => String
+      -> Manager
+      -> Source m Gist
+gists user m= let url = "https://api.github.com/users/" ++ user ++ "/gists"
+              in pagedRequest url m $= CL.map jsonToGist
