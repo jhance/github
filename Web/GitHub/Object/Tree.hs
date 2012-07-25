2bfaecb035069e62dd82874657b5404f85976ea3
Jared Hance <jaredhance@gmail.com>
2012-07-24 20:08:09 -0400

    Add RecursiveTree and RecursiveTreeFile.
    See documentation of the above two types for explanation of why they are
    different from Tree and TreeNode.

--- Web/GitHub/Object/Tree.hs
+++ Web/GitHub/Object/Tree.hs
@@ -6,7 +6,9 @@
 module Web.GitHub.Object.Tree
     (
     Tree(..),
-    TreeNode(..)
+    TreeNode(..),
+    RecursiveTree(..),
+    RecursiveTreeFile(..)
     )
 where
 
@@ -48,6 +50,51 @@ data TreeNode = FileNode {
     treeDirectoryUrl :: T.Text
     }
     deriving (Show, Read, Eq)
+
+-- | A data structure similar to a 'Tree' but instead of holding all subtrees
+-- and blobs of the tree, it holds all subblobs from recursively scanning
+-- all subtrees. It contains 'RecursiveTreeFile's instead of 'TreeNode's. This
+-- is the only difference between 'Tree' and 'RecursiveTree'.
+--
+-- A 'RecursiveTree' notably does not contain any information about any
+-- subtrees, so the information held by this data type is much different
+-- than the information held by the 'Tree' object.
+--
+-- Empty directories are not supported by Git so all possible subtree paths
+-- are able to be found by iterating over the filepaths.
+--
+-- A 'M.Map' is calculated from what is originally an associative 'V.Vector',
+-- so if iteration over all files and paths is desired this may be an extra
+-- and useless step. However, without this step, it is impossible to look up
+-- a file in less than O(n) time. This solution is not ideal for the case of
+-- iteration, but the extra processing does not have too much overhead.
+data RecursiveTree = RecursiveTree {
+    recursiveTreeSha :: T.Text,
+    recursiveTreeUrl :: T.Text,
+    recursiveTreePaths :: M.Map T.Text RecursiveTreeFile
+    }
+
+-- | A 'RecursiveTreeFile' differs from 'TreeNode' in that it always represents
+-- a file and its path is from the tree that it was recursively calculated
+-- with respect to. As such, a file in the git repository can have more than
+-- one associated 'RecursiveTreeFile' based on which 'RecursiveTree' was used
+-- to calculate it.
+--
+-- The path is a relative path but is allowed to contain directory traversal.
+-- It can be relative to many different locations so it is important not to
+-- assume that it is relative to the repository root unless you can guarantee
+-- this.
+--
+-- The best way to guarantee that it is relative to the root is to obtain the
+-- 'Tree' SHA from some 'Commit', and then request the 'RecursiveTree' for that
+-- 'Tree' SHA. Then, the list of 'RecursiveTreeFile's will all have all the
+-- possible files.
+data RecursiveTreeFile = RecursiveTreeFile {
+    recursiveTreeFileExecutable :: Bool,
+    recursiveTreeFileSize :: Integer,
+    recursiveTreeFileSha :: T.Text,
+    recursiveTreeFileUrl :: T.Text
+    }
 
 instance FromJSON Tree where
     parseJSON (Object o) = Tree
